ab2b45034ab729ad4331cacb00fe8ec516a437e6
SamTov <tovey.samuel@gmail.com>
2022-12-02 16:36:04 +0100

    fix formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 
 import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath("."))
+
 
 # -- Project information -----------------------------------------------------
 
